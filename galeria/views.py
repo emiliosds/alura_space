db91ac39c6a2a9e404ad6b64031cf07fed21d897
Emilio Santos <sto.emilio@gmail.com>
2023-07-04 22:26:17 -0300

    Termino do curso de persistência de dados

--- galeria/views.py
+++ galeria/views.py
@@ -8,3 +8,10 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', { "fotografia" : fotografia })
+
+def buscar(request):
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET["buscar"]
+        if nome_a_buscar:
+            fotografias = Fotografia.objects.order_by("-data_fotografia").filter(nome__icontains=nome_a_buscar)
+    return render(request, "galeria/buscar.html", { "cards" : fotografias })
